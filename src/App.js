7cb655d9ea15801127b04e615af033e15d3bf724
rishi <shyanthand19@gmail.com>
2023-03-20 12:15:19 +0530

    configure react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+ const [generate,Setgenerate]=useState(false)
+ 
+useEffect(()=>{
+    let title = document.querySelector('.title')
+    let textbody= document.querySelector('.textbody')
+      let rndint=Math.floor(Math.random() * (99 -0 + 1) + 0);    
+      fetch('https://jsonplaceholder.typicode.com/posts').then((response)=>{ return response.json() }).then((info)=>{
+         title.textContent= info[rndint].title
+         textbody.textContent=info[rndint].body
+      })
+
+}) 
+
+function handleclick(){
+    if(generate===false){
+      Setgenerate(true)
+    }
+    else {
+      Setgenerate(false)
+    }
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'><div className='name'>RANDOM TEXT GENERATOR</div></div>
+      <div className='content'>
+        <div className='text'><div className='title'>title</div> <div className='textbody'>body</div> </div>
+        <button className='generate' type='button' onClick={handleclick}>CLICK ME</button>
+      </div>
     </div>
   );
 }
